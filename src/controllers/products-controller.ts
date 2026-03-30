dd895f9dcb2bcbfadab440cfb6be50569971e791
Kauã Gabriell <kauagabriell64@gmail.com>
2026-03-30 15:50:56 -0300

    feat: implement product routes, controller, and database schema for product management

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
+import { knex } from '../database/knex';
+import { AppError } from '@/utils/AppError';
 
 class ProductController {
   async index(req: Request, res: Response, next: NextFunction) {
@@ -6,6 +9,21 @@ class ProductController {
       return res.json();
     } catch (error) {
       next(error);
+    }
+  }
+
+  async createProduct(req: Request, res: Response) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(4),
+        price: z.number().gt(0, { message: 'value must be grather than 0' }),
+      });
+      const product = bodySchema.parse(req.body);
+
+      await knex('products').insert(product);
+      return res.status(201).json(product);
+    } catch (e: any) {
+      return res.status(400).json({ message: e.message });
     }
   }
 }
