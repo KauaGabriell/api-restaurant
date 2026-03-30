b4ae0e9fa94ead4bb27e43f8c5acb25265ae4110
Kauã Gabriell <kauagabriell64@gmail.com>
2026-03-30 14:38:07 -0300

    feat: implement global error handler and configure knex database connection with zod validation

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction, response } from 'express';
-
 import { AppError } from '@/utils/AppError';
+import { ZodError } from 'zod';
 
 export function errorHandler(
   error: any,
@@ -12,5 +12,14 @@ export function errorHandler(
     return res.status(error.statusCode).json({ message: error.message });
   }
 
-  return response.status(500).json({message: error.message})
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .json({
+        message: `ERRO DE VALIDAÇÃO ZOD: ${error.message}`,
+        issues: error.format(),
+      });
+  }
+
+  return response.status(500).json({ message: error.message });
 }
