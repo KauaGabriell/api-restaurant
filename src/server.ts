571b2c2e70bbc8d426f2438e90eeddbeb2cea070
Kauã Gabriell <kauagabriell64@gmail.com>
2026-03-30 11:13:04 -0300

    chore: initialize project with typescript, express, and development configuration

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const PORT = 3333;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT} => http://localhost:${PORT}`);
+});
+
+app.get('/', async (req, res) => {
+  res.json('Hello Word, API Online');
+});
