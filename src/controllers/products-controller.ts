5774a670532f57e2ab1abf51e793f0b9039d9163
Kauã Gabriell <kauagabriell64@gmail.com>
2026-03-30 11:24:00 -0300

    feat: initialize express server with product controller and routing structure

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+
+class ProductController {
+  async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      return res.json();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { ProductController };
