5750b424a087d1a63e94617759f9f09ff2a63693
Kauã Gabriell <kauagabriell64@gmail.com>
2026-04-02 14:13:10 -0300

    fix: update table and column name in the tables_sessions migration

--- src/database/migrations/20260402141959_create-tables-sessions.ts
+++ src/database/migrations/20260402141959_create-tables-sessions.ts
@@ -1,14 +1,14 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('tables-sessions', (table) => {
+  await knex.schema.createTable('tables_sessions', (table) => {
     table.increments('id').primary();
     table.integer('table_id').notNullable().references('id').inTable('tables');
     table.timestamp('opened_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at');
+    table.timestamp('closed_at');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('tables-sessions');
+  await knex.schema.dropTable('tables_sessions');
 }
